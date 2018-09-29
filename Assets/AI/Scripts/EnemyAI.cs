921eeba1bf8630794d9f3b8c1b9e1deab6c5ad1b
denmire <ivan.ng.sm99@gmail.com>
2018-09-29 14:49:50 +0800

    basic ai movement

---
+++ Assets/AI/Scripts/EnemyAI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour, IEnemy, IDamageable {
+
+    public float speed { get; set; }
+    public float health { get; set; }
+    public float damage { get; set; }
+
+    public GameObject destinationObj;
+    public GameObject startPositionObj;
+
+    protected NavMeshAgent agent;
+    protected Vector3 destination;
+    protected Vector3 startPosition;
+    // Use this for initialization
+    void Start()
+    {
+        GetNavAgent();
+        destination = destinationObj.transform.position;
+        startPosition = startPositionObj.transform.position;
+        speed = agent.speed;
+        health = 100;
+        damage = 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public virtual void GetNavAgent()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+    }
+    
+    public void DealDamage(float damage)
+    {
+
+    }
+}
---
+++ Assets/AI/Scripts/Zombie.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Zombie : EnemyAI {
+
+	// Use this for initialization
+	void Start () {
+        base.GetNavAgent();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (destinationObj)
+            agent.SetDestination(destination);
+    }
+//    public override void GetNavAgent()
+//    {
+        
+//    }
+}
