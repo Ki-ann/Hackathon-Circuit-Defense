fd62c5da6ce939b294abc47cbe2c103f7d6a00cb
Daraxxus <sandvichisawesome@gmail.com>
2018-09-29 16:21:24 +0800

    Turret stuff

--- Assets/_OwnAssets/_GaryTest/Turrets/Turret.cs
+++ Assets/_OwnAssets/_GaryTest/Turrets/Turret.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public abstract class Turret : CircuitPart {
 	[Header ("Attack Stats")]
 	[Tooltip ("Attack Radius of tower")]
+	[SerializeField] private GameObject firePoint;
 	[SerializeField] private float attackRadius;
 	[SerializeField] private float attackSpeed;
 	[SerializeField] private float attackDamage;
 	private GameObject targetToAttack;
+
 	public override void Update () {
 		base.Update ();
-		if (isPlaced) 
-		{
+		if (isPlaced) {
 			//Turret Behvaiours
 			if (targetToAttack == null)
 				CheckRadius ();
 
 			if (targetToAttack != null)
-				Attack ();
+				TryAttack ();
 
 		}
 	}
@@ -27,6 +28,8 @@ public abstract class Turret : CircuitPart {
 		Collider[] colInRadius = Physics.OverlapSphere (visual.transform.position, attackRadius);
 
 		foreach (Collider col in colInRadius) {
+
+		
 			//check if col is enemy, if yes than target and attack
 			if (col.GetComponent<EnemyAI> () != null) {
 				targetToAttack = col.gameObject;
@@ -35,8 +38,20 @@ public abstract class Turret : CircuitPart {
 		}
 	}
 
-	void Attack () {
+	void TryAttack () {
+		LookAtTarget ();
+		RaycastHit hit;
+		if (Physics.Raycast (firePoint.transform.position, visual.transform.forward, out hit, attackRadius)) {
+			Debug.DrawRay (firePoint.transform.position, visual.transform.forward * attackRadius, Color.red);
+			if (hit.collider.GetComponent<EnemyAI> () != null) {
+				//Attack 
+				Debug.Log ("Die fiend");
+			}
+		}
+	}
 
+	void LookAtTarget () {
+		visual.transform.LookAt (targetToAttack.transform);
 	}
 
 	void OnDrawGizmos () {
