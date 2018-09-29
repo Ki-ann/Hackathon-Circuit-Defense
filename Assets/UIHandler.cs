031a0c034a16113d866b2281bb2bff055ba29203
Bernard Chua <bernardcks123@gmail.com>
2018-09-29 16:14:00 +0800

    Item type switching and UI handling

--- Assets/UIHandler.cs
+++ Assets/UIHandler.cs
@@ -5,4 +5,10 @@ using UnityEngine.UI;
 
 public class UIHandler : MonoBehaviour
 {
+    [SerializeField] private CircuitPlacer circuitPlacer;
+
+    public void UpdatePlacer(int itemType)
+    {
+        circuitPlacer.SetSelectedItemType((CircuitPlacer.ItemType)itemType);
+    }
 }
